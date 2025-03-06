f25d67eacb7212cca6edd9fa9d58df4ef5faef2f
kkami <kkami@KINGA>
2025-03-06 14:55:54 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            btn0 = new Button();
+            btnPrzecinek = new Button();
+            btnPlus = new Button();
+            btnMinus = new Button();
+            btnUkosnik = new Button();
+            btnGwiazka = new Button();
+            label1 = new Label();
+            textBox = new TextBox();
+            btnRownaSie = new Button();
+            btnC = new Button();
+            SuspendLayout();
+            // 
+            // btn1
+            // 
+            btn1.BackColor = Color.MintCream;
+            btn1.Location = new Point(38, 45);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(59, 56);
+            btn1.TabIndex = 0;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = false;
+            btn1.Click += button1_Click;
+            // 
+            // btn2
+            // 
+            btn2.BackColor = Color.MintCream;
+            btn2.Location = new Point(103, 45);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(59, 56);
+            btn2.TabIndex = 1;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = false;
+            // 
+            // btn3
+            // 
+            btn3.BackColor = Color.MintCream;
+            btn3.Location = new Point(168, 45);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(59, 56);
+            btn3.TabIndex = 2;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = false;
+            // 
+            // btn4
+            // 
+            btn4.BackColor = Color.MintCream;
+            btn4.Location = new Point(38, 107);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(59, 56);
+            btn4.TabIndex = 3;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = false;
+            // 
+            // btn5
+            // 
+            btn5.BackColor = Color.MintCream;
+            btn5.Location = new Point(103, 107);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(59, 56);
+            btn5.TabIndex = 4;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = false;
+            // 
+            // btn6
+            // 
+            btn6.BackColor = Color.MintCream;
+            btn6.Location = new Point(168, 107);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(59, 56);
+            btn6.TabIndex = 5;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = false;
+            // 
+            // btn7
+            // 
+            btn7.BackColor = Color.MintCream;
+            btn7.Location = new Point(38, 169);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(59, 56);
+            btn7.TabIndex = 6;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            btn8.BackColor = Color.MintCream;
+            btn8.Location = new Point(103, 169);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(59, 56);
+            btn8.TabIndex = 7;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            btn9.BackColor = Color.MintCream;
+            btn9.Location = new Point(169, 169);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(59, 56);
+            btn9.TabIndex = 8;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = false;
+            // 
+            // btn0
+            // 
+            btn0.BackColor = Color.MintCream;
+            btn0.Location = new Point(38, 231);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(124, 56);
+            btn0.TabIndex = 9;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = false;
+            // 
+            // btnPrzecinek
+            // 
+            btnPrzecinek.BackColor = Color.MintCream;
+            btnPrzecinek.Location = new Point(168, 231);
+            btnPrzecinek.Name = "btnPrzecinek";
+            btnPrzecinek.Size = new Size(59, 56);
+            btnPrzecinek.TabIndex = 10;
+            btnPrzecinek.Text = ",";
+            btnPrzecinek.UseVisualStyleBackColor = false;
+            // 
+            // btnPlus
+            // 
+            btnPlus.BackColor = SystemColors.ActiveBorder;
+            btnPlus.Location = new Point(234, 231);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(59, 118);
+            btnPlus.TabIndex = 11;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = false;
+            // 
+            // btnMinus
+            // 
+            btnMinus.BackColor = SystemColors.ActiveBorder;
+            btnMinus.Location = new Point(234, 169);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(59, 56);
+            btnMinus.TabIndex = 12;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = false;
+            btnMinus.Click += button12_Click;
+            // 
+            // btnUkosnik
+            // 
+            btnUkosnik.BackColor = SystemColors.ActiveBorder;
+            btnUkosnik.Location = new Point(234, 107);
+            btnUkosnik.Name = "btnUkosnik";
+            btnUkosnik.Size = new Size(59, 56);
+            btnUkosnik.TabIndex = 13;
+            btnUkosnik.Text = "/";
+            btnUkosnik.UseVisualStyleBackColor = false;
+            // 
+            // btnGwiazka
+            // 
+            btnGwiazka.BackColor = SystemColors.ActiveBorder;
+            btnGwiazka.Location = new Point(233, 45);
+            btnGwiazka.Name = "btnGwiazka";
+            btnGwiazka.Size = new Size(59, 56);
+            btnGwiazka.TabIndex = 14;
+            btnGwiazka.Text = "*";
+            btnGwiazka.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.MintCream;
+            label1.Location = new Point(169, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 15;
+            label1.Click += label1_Click;
+            // 
+            // textBox
+            // 
+            textBox.Location = new Point(38, 12);
+            textBox.Name = "textBox";
+            textBox.Size = new Size(255, 27);
+            textBox.TabIndex = 16;
+            textBox.TextAlign = HorizontalAlignment.Right;
+            textBox.TextChanged += textBox1_TextChanged;
+            // 
+            // btnRownaSie
+            // 
+            btnRownaSie.BackColor = SystemColors.InactiveCaption;
+            btnRownaSie.Location = new Point(38, 293);
+            btnRownaSie.Name = "btnRownaSie";
+            btnRownaSie.Size = new Size(124, 56);
+            btnRownaSie.TabIndex = 17;
+            btnRownaSie.Text = "=";
+            btnRownaSie.UseVisualStyleBackColor = false;
+            // 
+            // btnC
+            // 
+            btnC.BackColor = Color.FromArgb(224, 224, 224);
+            btnC.Location = new Point(169, 293);
+            btnC.Name = "btnC";
+            btnC.Size = new Size(58, 56);
+            btnC.TabIndex = 18;
+            btnC.Text = "C";
+            btnC.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(330, 373);
+            Controls.Add(btnC);
+            Controls.Add(btnRownaSie);
+            Controls.Add(textBox);
+            Controls.Add(label1);
+            Controls.Add(btnGwiazka);
+            Controls.Add(btnUkosnik);
+            Controls.Add(btnMinus);
+            Controls.Add(btnPlus);
+            Controls.Add(btnPrzecinek);
+            Controls.Add(btn0);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Button btn0;
+        private Button btnPrzecinek;
+        private Button btnPlus;
+        private Button btnMinus;
+        private Button btnUkosnik;
+        private Button btnGwiazka;
+        private Label label1;
+        private TextBox textBox;
+        private Button btnRownaSie;
+        private Button btnC;
+    }
+}
